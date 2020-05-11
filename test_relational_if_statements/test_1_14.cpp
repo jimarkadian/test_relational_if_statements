1fcaa7482c39a9a872697fee96fd545704e7b75f
jimarkadian <mccoyflowersjm@gmail.com>
2020-05-10 21:31:04 -0400

    Add project files.

---
+++ test_relational_if_statements/test_1_14.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+int main() {
+
+	if num1, num2;
+	
+	cout << "Enter two integers, and let's determine the relationships they satisfy:\n";
+
+	cin >> num1 >> num2;						// read two integers
+
+	if (num1 == num2)
+		cout << num1 << " is equal to " << num2 << endl;
+
+	if (num1 != num2)
+		cout << num1 << " is not equal to " << num2 << endl;
+
+	if (num1 < num2)
+		cout << num1 << " is less than " << num2 << endl;
+
+	if (num1 > num2)
+		cout << num1 << " is greater than " << num2 << endl;
+
+	if (num1 <= num2)
+		cout << num1 << " is less than or equal to " << num2 << endl;
+
+	if (num1 >= num2)
+		cout << num1 << " is greater than or equal to " << num2 << endl;
+
+
+
+
+}
